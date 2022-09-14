31b2e862125f8313b72f43f243a8f7c80261e8e9
VijaiCoder <vijaicoder@gmail.com>
2022-09-14 12:44:43 +0530

    first commit

---
+++ lib/pixel.dart
@@ -0,0 +1,23 @@
+// ignore_for_file: prefer_typing_uninitialized_variables
+
+import 'package:flutter/material.dart';
+
+class MyPixel extends StatelessWidget {
+
+  final color;
+
+  const MyPixel({super.key, this.color});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(2.0),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(6),
+        child: Container(
+          color: color,
+        ),
+      ),
+    );
+  }
+}
